a8ce7b0744dcf59b2b9e086d84d6fda385cf28b3
suyama <suyama@bengo4.com>
2021-02-16 03:43:44 +0900

    mod message

--- main.go
+++ main.go
@@ -16,5 +16,5 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	hello := os.Getenv("MY_VARIABLE")
-	fmt.Fprintf(w, "SERVICE2 MY_VARIABLE111:%s", hello)
+	fmt.Fprintf(w, "SERVICE2 MY_VARIABLE222:%s", hello)
 }
